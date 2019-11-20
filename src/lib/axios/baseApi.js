48d8c9f538a77cb13c6b83a7a7939d5f3ac12ea7
zwtzwx <384500364@qq.com>
2019-11-20 15:24:04 +0800

    修改

--- src/lib/axios/baseApi.js
+++ src/lib/axios/baseApi.js
@@ -17,6 +17,13 @@ switch (hostName) {
       // BASE_API_URL: 'https://dev-jiyun.tomtiddler.top/jiyun-api/admin'
     }
     break
+  // v2 正式服
+  case 'jiyun-v2.haiouoms.com':
+    baseUrl = {
+      BASE_API_URL: 'https://api-jiyun-v2.haiouoms.com/api/admin',
+      IMAGE_URL: 'https://api-jiyun-v2.haiouoms.com'
+    }
+    break
   // 测试服
   case 'jiyun-test-admin-v2.nle-tech.com':
     baseUrl = {
