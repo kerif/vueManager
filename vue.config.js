3723df6237c711fe5c3b79cb8d61215bbba85d2c
zwtzwx <384500364@qq.com>
2020-03-17 16:44:24 +0800

    生产环境 console 配置

--- vue.config.js
+++ vue.config.js
@@ -8,5 +8,10 @@ module.exports = {
       warnings: true,
       error: true
     }
+  },
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true
+    }
   }
 }
