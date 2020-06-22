a1804858c7e47b8be6f0da26875ae086321f14fe
lsy <598348605@qq.com>
2020-06-22 17:39:04 +0800

    Add header router list

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import tagsView from './module/tagsView'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  modules: { tagsView },
   state: {
     isPermissionFilter: false,
     fileterAfterRouterMap: [],
