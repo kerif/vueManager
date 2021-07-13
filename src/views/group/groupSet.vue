22b2f118c019584a83cf16b175f748d5fb6ee1eb
vanora1111 <1270754175@qq.com>
2021-07-13 10:45:02 +0800

    新增简繁体

--- src/views/group/groupSet.vue
+++ src/views/group/groupSet.vue
@@ -113,8 +113,8 @@
         <span class="el-icon-question icon-info"></span>
       </el-tooltip>
       <div slot="footer">
-        <el-button @click="dialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="submitGroup">确 定</el-button>
+        <el-button @click="dialogVisible = false">{{ $t('取消') }}</el-button>
+        <el-button type="primary" @click="submitGroup">{{ $t('确定') }}</el-button>
       </div>
     </el-dialog>
   </div>
