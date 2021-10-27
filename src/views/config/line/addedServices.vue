6bb6880e93ce9e7e1497f8f70c9812c8df9fdee8
WHXRR <1147232219@qq.com>
2021-10-27 14:11:49 +0800

    修改渠道增值服务分页

--- src/views/config/line/addedServices.vue
+++ src/views/config/line/addedServices.vue
@@ -4,15 +4,53 @@
       *{{ $t('可在此界面设置渠道专用增值服务，如加固费、打包费、保价费、偏远地区配送费等。') }}
     </h5>
     <div class="added-services">
-      <vxe-modal
-        v-model="dialogVisible"
-        @hide="closeDialog"
-        id="myModal6"
-        width="70%"
-        show-footer
-        esc-closable
-        type="modal"
+      <vxe-grid
+        ref="xGrid"
+        resizable
+        auto-resize
+        show-overflow
+        highlight-hover-row
+        :export-config="{}"
+        :tooltip-config="{ enterable: true }"
+        :data="servicesList"
+        v-bind="gridOptions"
+      >
+        <template #toolbar_buttons>
+          <div style="display: flex; gap: 10px">
+            <el-button size="small" type="primary" plain @click="addServices">{{
+              $t('新增')
+            }}</el-button>
+            <el-upload
+              class="upload-demo"
+              action=""
+              :http-request="uploadBaleImg"
+              :show-file-list="false"
+            >
+              <el-button size="small" type="warning" plain>{{ $t('导入') }}</el-button>
+            </el-upload>
+            <el-button size="small" @click="exportDataEvent" type="success" plain>{{
+              $t('导出')
+            }}</el-button>
+          </div>
+        </template>
+        <template #num1_header="{ column }">
+          <el-tooltip class="item" effect="dark" :content="column.params.tips" placement="top">
+            <div class="function">
+              <span>{{ column.title }}</span>
+              <span class="func-icon">
+                <i class="el-icon-edit-outline" @click="editRowEvent(column)"></i>
+                <i class="el-icon-delete" @click="deleteServices(column)"></i>
+              </span>
+            </div>
+          </el-tooltip>
+        </template>
+      </vxe-grid>
+      <el-dialog
+        class="dialog-container"
         :title="title"
+        :visible.sync="dialogVisible"
+        width="70%"
+        @close="handleClose"
       >
         <div>
           <el-form ref="form" :model="form" label-width="120px">
@@ -86,79 +124,12 @@
                 style="width: 300px; padding-left: 20px"
               ></el-input>
             </el-form-item>
-            <!-- <el-form-item label="English">
-            <el-input
-              v-model="langForm.en_US"
-              :placeholder="$t('请输入')"
-              style="width: 217px"
-            ></el-input>
-            <el-input
-              v-model="form.remark"
-              :placeholder="$t('请输入English费用说明')"
-              style="width: 300px"
-            ></el-input>
-          </el-form-item>
-          <el-form-item :label="$t('繁体中文')">
-            <el-input
-              v-model="langForm.zh_TW"
-              :placeholder="$t('请输入')"
-              style="width: 217px"
-            ></el-input>
-            <el-input
-              v-model="form.remark"
-              :placeholder="$t('请输入繁体中文费用说明')"
-              style="width: 300px"
-            ></el-input>
-          </el-form-item> -->
           </el-form>
         </div>
-        <span slot="footer">
+        <span slot="footer" class="dialog-footer">
           <el-button type="primary" @click="save">{{ $t('确 定') }}</el-button>
         </span>
-      </vxe-modal>
-      <vxe-grid
-        ref="xGrid"
-        resizable
-        auto-resize
-        show-overflow
-        highlight-hover-row
-        :export-config="{}"
-        :tooltip-config="{ enterable: true }"
-        :data="servicesList"
-        v-bind="gridOptions"
-      >
-        <template #toolbar_buttons>
-          <div style="display: flex; gap: 10px">
-            <el-button size="small" type="primary" plain @click="addServices">{{
-              $t('新增')
-            }}</el-button>
-            <el-upload
-              class="upload-demo"
-              action=""
-              :http-request="uploadBaleImg"
-              :show-file-list="false"
-            >
-              <el-button size="small" type="warning" plain>{{ $t('导入') }}</el-button>
-            </el-upload>
-            <el-button size="small" @click="exportDataEvent" type="success" plain>{{
-              $t('导出')
-            }}</el-button>
-          </div>
-        </template>
-        <template #num1_header="{ column }">
-          <el-tooltip class="item" effect="dark" :content="column.params.tips" placement="top">
-            <div class="function">
-              <span>{{ column.title }}</span>
-              <span class="func-icon">
-                <i class="el-icon-edit-outline" @click="editRowEvent(column)"></i>
-                <i class="el-icon-delete" @click="deleteServices(column)"></i>
-              </span>
-              <!-- <vxe-button type="text" @click="editRowEvent(column)">{{ $t('编辑') }}</vxe-button> -->
-              <!-- <vxe-button type="text" @click="deleteServices(column)">{{ $t('删除') }}</vxe-button> -->
-            </div>
-          </el-tooltip>
-        </template>
-      </vxe-grid>
+      </el-dialog>
     </div>
   </div>
 </template>
@@ -255,7 +226,7 @@ export default {
     }
   },
   created() {
-    this.getServicesList()
+    this.getList()
     this.getLanguageList()
   },
   methods: {
@@ -275,7 +246,7 @@ export default {
       })
     },
     // 获取增值服务列表
-    getServicesList() {
+    getList() {
       this.gridOptions.columns = [
         {
           type: 'seq',
@@ -296,7 +267,7 @@ export default {
           minWidth: 400
         }
       ]
-      this.$request.getServicesList(this.$route.params.id).then(res => {
+      this.$request.getServicesList(this.$route.params.id, { size: 200 }).then(res => {
         if (res.ret) {
           this.localization = res.localization
           this.servicesList = res.data.map(item => {
@@ -341,7 +312,6 @@ export default {
                 }
                 this.gridOptions.columns.push({
                   field: `service_${ele.id}`,
-                  // title: ele.name + '(' + this.localization.currency_unit + ')',
                   title: ele.name,
                   params: {
                     tips: this.content
@@ -370,33 +340,35 @@ export default {
       this.detailsList = this.servicesList
       this.serviceId = +row.property.split('_')[1]
       // 获取编辑时表格数据
-      this.$request.getServicesDetails(this.$route.params.id, this.serviceId).then(res => {
-        if (res.ret) {
-          this.form.name = res.data.name
-          this.form.type = res.data.type
-          this.form.is_forced = res.data.is_forced
-          this.form.remark = res.data.remark
-          this.form.name_translations = res.data.name_translations
-          this.form.remark_translations = res.data.remark_translations
-          this.detailsList = res.data.prices.map(item => {
-            const name = item.region_name
-            const areas = item.areas
-              .map(item => item.country_name + item.area_name + item.sub_area_name)
-              .join('、')
-            return {
-              ...item,
-              name,
-              areas
-            }
-          })
-        }
-      })
+      this.$request
+        .getServicesDetails(this.$route.params.id, this.serviceId, { size: 200 })
+        .then(res => {
+          if (res.ret) {
+            this.form.name = res.data.name
+            this.form.type = res.data.type
+            this.form.is_forced = res.data.is_forced
+            this.form.remark = res.data.remark
+            this.form.name_translations = res.data.name_translations
+            this.form.remark_translations = res.data.remark_translations
+            this.detailsList = res.data.prices.map(item => {
+              const name = item.region_name
+              const areas = item.areas
+                .map(item => item.country_name + item.area_name + item.sub_area_name)
+                .join('、')
+              return {
+                ...item,
+                name,
+                areas
+              }
+            })
+          }
+        })
     },
     // 新增增值服务
     addServices() {
       this.title = this.$t('新增增值费用')
       this.dialogVisible = true
-      this.getServicesList()
+      this.getList()
       this.detailsList = this.servicesList
     },
     // 删除增值服务
@@ -414,7 +386,7 @@ export default {
               title: this.$t('操作成功'),
               message: res.msg
             })
-            this.getServicesList()
+            this.getList()
           } else {
             this.$message({
               message: res.msg,
@@ -452,7 +424,7 @@ export default {
             message: res.msg,
             type: 'success'
           })
-          this.getServicesList()
+          this.getList()
         } else {
           this.$notify({
             title: this.$t('操作失败'),
@@ -492,7 +464,7 @@ export default {
             })
             this.dialogVisible = false
             this.detailsList = []
-            this.getServicesList()
+            this.getList()
             // this.$router.go(0)
           } else {
             this.$message({
@@ -510,7 +482,7 @@ export default {
               message: res.msg
             })
             this.dialogVisible = false
-            this.getServicesList()
+            this.getList()
           } else {
             this.$message({
               message: res.msg,
@@ -521,7 +493,7 @@ export default {
       }
     },
     // 关闭弹窗
-    closeDialog() {
+    handleClose() {
       this.detailsList.length = 0
       this.serviceId = 0
       this.form.type = ''
