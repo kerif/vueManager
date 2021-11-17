7adcfd3ed1bf1394dc13f009c08daa0687472bc5
cxyzl-45 <2250937996@qq.com>
2021-11-17 18:14:09 +0800

    密码加密

--- src/views/login.vue
+++ src/views/login.vue
@@ -278,8 +278,7 @@
   </div>
 </template>
 <script>
-// import JSEncrypt from 'jsencrypt'
-// import { excrypt } from '@/utils/rsaEncrypt'
+import JSEncrypt from 'jsencrypt'
 export default {
   name: 'login',
   data() {
@@ -405,6 +404,7 @@ export default {
     },
     // 登录
     onLogin() {
+      this.userInfo.password = this.excrypt(this.userInfo.password)
       if (!this.userInfo.username.trim()) {
         return this.$message.info(this.$t('请输入邮箱或手机号'))
       } else if (!this.userInfo.password.trim()) {
@@ -658,21 +658,21 @@ export default {
           this.$message.error(res.msg)
         }
       })
+    },
+    excrypt(data) {
+      const publicKey = `-----BEGIN PUBLIC KEY-----
+    MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwfxIAFQxipEEKJMc4RhD
+    LJQ7WxMGWLpinEEbJbMoXfSqlDjhxfVx5//itbwWmIKM1A8n0MTerudTvjdDmG9k
+    37+EDS/dcYmxZ7KCqEp4teBsRmg8v9EgnblgkEVpwTA7M5zgldMFJnearrBx2IwL
+    hWqK3IKADSiLcvrO25crz89bkPq2jgmi2QOsKEIATOErrkyFTxRVYqw8/UB0q5Za
+    /KWAE9xTgU7+oteoRnI7ZdXlJZ1ENyMPN+DAT4WZoR+nWmKCVPFNgwdN2b3D8Rum
+    6L1bQcOMbK2wS9Ooc1uu6+AfT8/HTODNmMnBf4/LBWpwjOLUhz1YLaeNnYqgpUzS
+    HwIDAQAB
+    -----END PUBLIC KEY-----`
+      const encryptor = new JSEncrypt()
+      encryptor.setPublicKey(publicKey)
+      return encryptor.encrypt(data)
     }
-    //     excrypt(data) {
-    //       const publicKey = `-----BEGIN PUBLIC KEY-----
-    // MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwfxIAFQxipEEKJMc4RhD
-    // LJQ7WxMGWLpinEEbJbMoXfSqlDjhxfVx5//itbwWmIKM1A8n0MTerudTvjdDmG9k
-    // 37+EDS/dcYmxZ7KCqEp4teBsRmg8v9EgnblgkEVpwTA7M5zgldMFJnearrBx2IwL
-    // hWqK3IKADSiLcvrO25crz89bkPq2jgmi2QOsKEIATOErrkyFTxRVYqw8/UB0q5Za
-    // /KWAE9xTgU7+oteoRnI7ZdXlJZ1ENyMPN+DAT4WZoR+nWmKCVPFNgwdN2b3D8Rum
-    // 6L1bQcOMbK2wS9Ooc1uu6+AfT8/HTODNmMnBf4/LBWpwjOLUhz1YLaeNnYqgpUzS
-    // HwIDAQAB
-    // -----END PUBLIC KEY-----`
-    //       const encryptor = new JSEncrypt()
-    //       encryptor.setPublicKey(publicKey)
-    //       return encryptor.encrypt(data)
-    //     }
   }
 }
 </script>
