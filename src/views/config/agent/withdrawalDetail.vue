7bb7980366e79451190615d4c6032e1ff96578df
cxyzl-45 <2250937996@qq.com>
2022-11-29 18:08:11 +0800

    添加微信提现明细

--- src/views/config/agent/withdrawalDetail.vue
+++ src/views/config/agent/withdrawalDetail.vue
@@ -82,6 +82,15 @@
         </template>
       </el-table>
     </div>
+    <div class="receiveMSg" v-if="form.status === 1 && wechatList.length">
+      <h4>{{ $t('微信提现明细') }}</h4>
+      <el-table class="data-list" border stripe v-loading="tableLoading" :data="wechatList">
+        <el-table-column type="index"></el-table-column>
+        <el-table-column prop="batch_id" :label="$t('批次号')"></el-table-column>
+        <el-table-column prop="created_at" :label="$t('时间')"></el-table-column>
+        <el-table-column prop="status_name" :label="$t('状态')"></el-table-column>
+      </el-table>
+    </div>
     <div class="footer-btn" v-if="this.$route.params.state === 'review'">
       <el-button type="danger" @click="reviewReject">{{ $t('审核拒绝') }}</el-button>
       <el-button type="primary" @click="reviewPass">{{ $t('审核通过') }}</el-button>
@@ -145,7 +154,8 @@ export default {
       amount: '',
       imgVisible: false,
       imgSrc: '',
-      tableLoading: false
+      tableLoading: false,
+      wechatList: []
     }
   },
   created() {
@@ -159,6 +169,7 @@ export default {
           this.form = res.data
           this.withdrawList = res.data.commissions
           this.amount = res.data.amount
+          this.wechatList = res.data.wechat_transfer_records
           this.localization = res.localization
         } else {
           this.$notify({
