fc3ce1de525c8ed57e12d4072fb1dcf5d7f1e635
cxyzl-45 <2250937996@qq.com>
2023-01-13 17:58:19 +0800

    调整订单作废3

--- src/components/dialog/invalidOrder.vue
+++ src/components/dialog/invalidOrder.vue
@@ -48,7 +48,7 @@
           class="input-sty"
           v-model="ruleForm.refund_amount"
           :disabled="
-            (status === 1 && paymentStatus === 2) || activeName === '1' || activeName === '2'
+            (activeName === '1' && !(status === 1 && paymentStatus === 2)) || activeName === '2'
           "
         >
         </el-input>
